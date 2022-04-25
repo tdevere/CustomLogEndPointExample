6fa3014d42ca95df3a5fd33459424559c1488b65
tdevere <tdevere@microsoft.com>
2022-04-25 17:51:59 -0500

    Add project files.

---
+++ CustomLogEndPointExample/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace CustomLogEndPointExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppCenter.LogLevel = LogLevel.Verbose;
+            AppCenter.SetLogUrl("https://azurefunctionproxyforappcenter.azurewebsites.net");
+            AppCenter.Start("e68d0c3c-489c-4a9f-b366-c026d78ed851",
+                   typeof(Analytics), typeof(Crashes));
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ CustomLogEndPointExample/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+
+namespace CustomLogEndPointExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, string> properties;
+        Guid sessionGuid = Guid.NewGuid();
+        public MainWindow()
+        {
+            properties = new Dictionary<string, string>() { { sessionGuid.ToString(), "Session Start" }, { "TDEVERE", "KICKING IT" } };
+            SendEvt(sessionGuid.ToString(), properties);
+            InitializeComponent();
+        }
+
+        private void btnSendEvent_Click(object sender, RoutedEventArgs e)
+        {
+            SendEvt("btnSendEvent_Click");
+        }
+
+        private void SendEvt(string eventName, Dictionary<string,string> properties)
+        {
+            try
+            {
+                Analytics.TrackEvent(eventName, properties);
+            }
+            catch (Exception ex)
+            {
+                Crashes.TrackError(ex);
+            }
+        }
+
+        private void SendEvt(string eventName)
+        {
+            try
+            {
+                properties = new Dictionary<string, string>() { { sessionGuid.ToString(), DateTime.Now.ToLongTimeString() } };
+                Analytics.TrackEvent(eventName, properties);
+            }
+            catch (Exception ex)
+            {
+                Crashes.TrackError(ex);
+            }
+        }
+    }
+}
